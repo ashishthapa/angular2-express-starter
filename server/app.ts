a44b52dfb0074cba57b13a879f82c31e6738fbaa
ashishthapa <acis.thapa@gmail.com>
2017-08-13 11:25:03 +0300

    using scss for mixins: discard tables for div

--- server/app.ts
+++ server/app.ts
@@ -13,17 +13,9 @@ const app: express.Application = express();
 
 
 app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://tuftuf.gambitlabs.fi/feed.txt');
-
-  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Pass to next layer of middleware
   next();
 });
 app.disable("x-powered-by");
